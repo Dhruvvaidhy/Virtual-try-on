f7fe8ec2c0b7e88c74ae5b5b11d841c20be92f72
Dhruvvaidhy <dhruv786.oceanmtech@gmail.com>
2025-02-15 10:42:24 +0530

    e

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
